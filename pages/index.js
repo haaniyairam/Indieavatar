a48019a9f1211817ad6a12c2fc4a858c06eba524
haaniyairam <haaniyairam@gmail.com>
2023-03-14 21:14:40 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [input, setInput] = useState('');
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>GenieAvatar: Grant Your Image Wishes| buildspace</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>GenieAvatar: Grant Your Image Wishes</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Hi, I'm Hira, and you'll never forget it.
+            </h2>
           </div>
+          <div className="prompt-container">
+        <input className="prompt-box" value={input}/>
+        </div>
         </div>
       </div>
       <div className="badge-container grow">
