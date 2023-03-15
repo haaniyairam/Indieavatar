8bfe2cf438dac5f80420cf972d2ba13f78bb67b7
haaniyairam <haaniyairam@gmail.com>
2023-03-15 12:42:22 +0530

    cool

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,22 +1,35 @@
 const generateAction = async (req, res) => {
-    console.log('Received request');
+  console.log("Received request");
 
-    const input = JSON.parse(req.body).input;
+  const input = JSON.parse(req.body).input;
 
-    const response = await fetch(
-        `https://api-inference.huggingface.co/models/Haaniya-Iram17/Haaniya-Iram17/sd-1-5-hira`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-          body: JSON.stringify({
-            inputs: input,
-          }),
-        }
-      );
-  };
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/buildspace/ai-avatar-generator`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        inputs: input,
+      }),
+    }
+  );
 
+  if (response.ok) {
+    const buffer = await response.arrayBuffer();
+    // Convert to base64
+    const base64 = bufferToBase64(buffer);
+    // Make sure to change to base64
+    res.status(200).json({ image: base64 });
+  } else if (response.status === 503) {
+    const json = await response.json();
+    res.status(503).json(json);
+  } else {
+    const json = await response.json();
+    res.status(response.status).json({ error: response.statusText });
+  }
+};
 
-  export default generateAction;
+export default generateAction;
