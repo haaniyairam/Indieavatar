4570020531af224187d2a902105bfaa3a47d830d
haaniyairam <haaniyairam@gmail.com>
2023-03-15 08:35:49 +0530

    cool

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,5 +1,21 @@
 const generateAction = async (req, res) => {
-    console.log('Received request')
-  }
-  
+    console.log('Received request');
+
+    const input = JSON.parse(req.body).input;
+
+    const response = await fetch(
+        `https://api-inference.huggingface.co/models/Haaniya-Iram17/Haaniya-Iram17/sd-1-5-hira`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            inputs: input,
+          }),
+        }
+      );
+  };
+
   export default generateAction;
