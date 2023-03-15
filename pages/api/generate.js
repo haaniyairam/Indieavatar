fb4c60274bc2b8ac3b3a6ea323ae4e32d3a981dc
haaniyairam <haaniyairam@gmail.com>
2023-03-15 13:15:04 +0530

    cool

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,10 +1,3 @@
-const bufferToBase64 = (buffer) => {
-  let arr = new Uint8Array(buffer);
-  const base64 = btoa(
-    arr.reduce((data, byte) => data + String.fromCharCode(byte), "")
-  );
-  return `data:image/png;base64,${base64}`;
-};
 
 const generateAction = async (req, res) => {
   console.log("Received request");
@@ -24,20 +17,5 @@ const generateAction = async (req, res) => {
       }),
     }
   );
-
-  if (response.ok) {
-    const buffer = await response.arrayBuffer();
-    // Convert to base64
-    const base64 = bufferToBase64(buffer);
-    // Make sure to change to base64
-    res.status(200).json({ image: base64 });
-  } else if (response.status === 503) {
-    const json = await response.json();
-    res.status(503).json(json);
-  } else {
-    const json = await response.json();
-    res.status(response.status).json({ error: response.statusText });
-  }
-};
 
 export default generateAction;
